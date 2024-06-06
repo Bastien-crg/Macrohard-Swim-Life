c0861d406f8d8f0301cdaf6d2fa528ee0ba33f11
MickahL <mv011@hotmail.fr>
2024-06-06 18:31:54 +0200

    added rain option in settings

--- Assets/Scripts/Rain.cs
+++ Assets/Scripts/Rain.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,27 @@ public class Rain : MonoBehaviour
 {
     public Transform target;
     public float offsetY;
+    private ParticleSystem rain;
+    private AudioSource rainAudio;
 
+    private void Start()
+    {
+        rain = GetComponentInChildren<ParticleSystem>();
+        rainAudio = GetComponentInChildren<AudioSource>();
+    }
+
+    public void StopRain(Component sender, object data)
+    {
+        rain.Stop(withChildren:true);
+        rainAudio.Stop();
+    }
+
+    public void StartRain(Component sender, object data)
+    {
+        rain.Play(withChildren:true);
+        rainAudio.Play();
+    }
+    
     // Update is called once per frame
     void Update()
     {
