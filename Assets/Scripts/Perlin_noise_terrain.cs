e0d28aa41694d63b54238b0102e2e7aa4fd91c7a
Bastien <bastien.corgnac@gmail.com>
2024-06-07 22:04:38 +0200

    fix map

--- Assets/Scripts/Perlin_noise_terrain.cs
+++ Assets/Scripts/Perlin_noise_terrain.cs
@@ -49,7 +49,24 @@ public class Perlin_noise_terrain : MonoBehaviour
     {
         terrainData.heightmapResolution = width ;
         terrainData.size = new Vector3(width, depth, height);
-        terrainData.SetHeights(0, 0, GenerateHeights());
+        float[,] n = GenerateHeights();
+        terrainData.SetHeights(0, 0, n);
+
+        Debug.Log(n[0, 0] + " " + n[0, 1] + " " + n[0, 2] + " " + n[0, 3] + " " + n[0, 4] + " " + n[0, 5] + " " + n[0, 6] );
+        Debug.Log(n[1, 0] + " " + n[1, 1] + " " + n[1, 2] + " " + n[1, 3] + " " + n[1, 4] + " " + n[1, 5] + " " + n[1, 6] );
+        Debug.Log(n[2, 0] + " " + n[2, 1] + " " + n[2, 2] + " " + n[2, 3] + " " + n[2, 4] + " " + n[2, 5] + " " + n[2, 6] );
+        Debug.Log(n[3, 0] + " " + n[3, 1] + " " + n[3, 2] + " " + n[3, 3] + " " + n[3, 4] + " " + n[3, 5] + " " + n[3, 6]);
+        Debug.Log(n[4, 0] + " " + n[4, 1] + " " + n[4, 2] + " " + n[4, 3] + " " + n[4, 4] + " " + n[4, 5] + " " + n[4, 6]);
+
+
+        for (int i = 0; i < 10; i++)
+        {
+            Debug.Log("ligne : " + i);
+            for (int k = 0; k < 10; k++)
+            {
+                Debug.Log(i +" "+k + " : " + n[i,k]);
+            }
+        }
         return terrainData;
     }
 
--- Assets/Scripts/ProceduralGeneration/Map/MapGenerator.cs
+++ Assets/Scripts/ProceduralGeneration/Map/MapGenerator.cs
@@ -18,14 +18,20 @@ public class MapGenerator : MonoBehaviour {
 	public bool autoUpdate;
 
 	public void GenerateMap() {
-		float[,] noiseMap = Noiser.GenerateNoiseMap (mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+		Debug.Log(mapWidth + " " + mapHeight + " " + seed + " " + noiseScale + " " + octaves + " " + persistance + " " + lacunarity);
+		float[,] n = Noiser.GenerateNoiseMap (mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+		Debug.Log(n[0, 0] + " " + n[0, 1] + " " + n[0, 2] + " " + n[0, 3] + " " + n[0, 4] + " " + n[0, 5] + " " + n[0, 6]);
+		Debug.Log(n[1, 0] + " " + n[1, 1] + " " + n[1, 2] + " " + n[1, 3] + " " + n[1, 4] + " " + n[1, 5] + " " + n[1, 6]);
+		Debug.Log(n[2, 0] + " " + n[2, 1] + " " + n[2, 2] + " " + n[2, 3] + " " + n[2, 4] + " " + n[2, 5] + " " + n[2, 6]);
+		Debug.Log(n[3, 0] + " " + n[3, 1] + " " + n[3, 2] + " " + n[3, 3] + " " + n[3, 4] + " " + n[3, 5] + " " + n[3, 6]);
+		Debug.Log(n[4, 0] + " " + n[4, 1] + " " + n[4, 2] + " " + n[4, 3] + " " + n[4, 4] + " " + n[4, 5] + " " + n[4, 6]);
 
 
 		MapDisplay display = FindObjectOfType<MapDisplay> ();
-		display.DrawNoiseMap (noiseMap);
+		display.DrawNoiseMap (n);
 
 		TreeMapDisplay treeDisplay = FindObjectOfType<TreeMapDisplay>();
-		treeDisplay.DrawTreeNoiseMap(noiseMap);
+		treeDisplay.DrawTreeNoiseMap(n);
 	}
 
 	void OnValidate() {
